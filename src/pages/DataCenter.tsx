f23942908d0ae5b4e69d9924f96faab937df35ad
goldmaster401093221 <goldmaster401093221@gmail.com>
2025-07-18 11:35:59 +0300

    update: comment icon instead of eye

--- src/pages/DataCenter.tsx
+++ src/pages/DataCenter.tsx
@@ -308,7 +308,7 @@ const DataCenter = () => {
           <div className="bg-white rounded-lg shadow">
             <div className="overflow-x-auto">
               <table className="w-full">
-                <thead className="bg-gray-50 border-b border-gray-200">
+                <thead className="bg-gray-300 border-b border-gray-200">
                   <tr>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       File name
